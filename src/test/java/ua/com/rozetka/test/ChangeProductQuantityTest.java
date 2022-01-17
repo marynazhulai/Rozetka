fae5618fc0b22679302dd52246255164dec65562
marynazhulai <marynazhulai@gmail.com>
2022-01-17 23:04:16 +0200

    screenshot on fail added

--- src/test/java/ua/com/rozetka/test/ChangeProductQuantityTest.java
+++ src/test/java/ua/com/rozetka/test/ChangeProductQuantityTest.java
@@ -17,7 +17,7 @@ public class ChangeProductQuantityTest extends BaseTest {
         addProductToCartBO.addProductToCartFromComputersAndNotebooks();
         CartPage cartPage = new CartPage();
         cartPage.addOneMoreProduct();
-        Assert.assertEquals(cartPage.getCountOfProduct(), 22, "Count of product doesn`t match");
+        Assert.assertEquals(cartPage.getCountOfProduct(), 2, "Count of product doesn`t match");
         ProductOverviewPage productOverviewPage = new ProductOverviewPage();
         int expectedSubtotal = productOverviewPage.getProductPrice() * 2;
         Assert.assertEquals(productOverviewPage.getProductPrice() * cartPage.getCountOfProduct(), expectedSubtotal, "Subtotal in cart doesn`t match");
